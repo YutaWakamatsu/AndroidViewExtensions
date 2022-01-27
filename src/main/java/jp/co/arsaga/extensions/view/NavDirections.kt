5fb6f1529bdff3b0b209ff401e0123191c152efd
内田正輝 <masaki.u@arsaga.jp>
2022-01-27 14:58:33 +0900

    画面遷移時に遷移前処理とエラー時のハンドリングをできるように修正

--- src/main/java/jp/co/arsaga/extensions/view/NavDirections.kt
+++ src/main/java/jp/co/arsaga/extensions/view/NavDirections.kt
@@ -4,13 +4,40 @@ import android.app.Activity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import timber.log.Timber
 
-fun NavDirections.popBackStack(fragment: Fragment, isInclusive: Boolean = true) {
-    fragment.findNavController().popBackStack(actionId, isInclusive)
+fun NavDirections.popBackStack(
+    fragment: Fragment,
+    isInclusive: Boolean = true,
+    preAction: () -> Unit = {},
+    onError: (Throwable) -> Unit = { Timber.e(it) },
+) {
+    preAction()
+    runCatching {
+        fragment.findNavController().popBackStack(actionId, isInclusive)
+    }.onFailure(onError)
 }
-fun NavDirections.navigate(fragment: Fragment, navigateAnimation: NavigateAnimationType? = null) {
-    fragment.findNavController().navigate(this, animateNavOptionsFactory(navigateAnimation))
+
+fun NavDirections.navigate(
+    fragment: Fragment,
+    navigateAnimation: NavigateAnimationType? = null,
+    preAction: () -> Unit = {},
+    onError: (Throwable) -> Unit = {},
+) {
+    preAction()
+    runCatching {
+        fragment.findNavController().navigate(this, animateNavOptionsFactory(navigateAnimation))
+    }.onFailure(onError)
 }
-fun NavDirections.navigate(activity: Activity, navigateAnimation: NavigateAnimationType? = null) {
-    activity.getNavController()?.navigate(this, animateNavOptionsFactory(navigateAnimation))
+
+fun NavDirections.navigate(
+    activity: Activity,
+    navigateAnimation: NavigateAnimationType? = null,
+    preAction: () -> Unit = {},
+    onError: (Throwable) -> Unit = {},
+) {
+    preAction()
+    runCatching {
+        activity.getNavController()?.navigate(this, animateNavOptionsFactory(navigateAnimation))
+    }.onFailure(onError)
 }
