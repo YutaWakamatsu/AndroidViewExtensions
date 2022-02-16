16a1066fd6471a3782d3cacc9a46a9a84e4b095e
内田正輝 <masaki.u@arsaga.jp>
2022-02-16 18:40:51 +0900

    ページ毎のJavascript有効/無効のハンドリングに漏れがあったので修正

--- src/main/java/jp/co/arsaga/extensions/view/baseLayout/BaseWebViewFragment.kt
+++ src/main/java/jp/co/arsaga/extensions/view/baseLayout/BaseWebViewFragment.kt
@@ -1,14 +1,20 @@
 package jp.co.arsaga.extensions.view.baseLayout
 
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import jp.co.arsaga.extensions.view.R
+import timber.log.Timber
 
 abstract class BaseWebViewFragment : Fragment() {
 
@@ -16,15 +22,36 @@ abstract class BaseWebViewFragment : Fragment() {
 
     protected fun getSwipeView(): SwipeRefreshLayout? = view?.findViewById(R.id.reloadWebView)
 
-    open class Client(private val swipeViewQuery: () -> SwipeRefreshLayout?) : WebViewClient() {
+    open class Client(
+        private val swipeViewQuery: () -> SwipeRefreshLayout?,
+        private val isEnabledJavascript: (view: WebView?, url: String?) -> Boolean,
+    ) : WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            view?.settings?.javaScriptEnabled = isEnabledJavascript(view, url)
+            super.onPageStarted(view, url, favicon)
+        }
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            Timber.d("URL::${request?.url}")
+            view?.settings?.javaScriptEnabled = isEnabledJavascript(view, request?.url?.toString())
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
             swipeViewQuery()?.isRefreshing = false
         }
     }
 
+    abstract fun isEnabledJavascript(view: WebView?, url: String?): Boolean
+
     open val webViewClient: Client by lazy {
-        Client(::getSwipeView)
+        Client(::getSwipeView, ::isEnabledJavascript)
     }
 
     override fun onCreateView(
@@ -33,14 +60,26 @@ abstract class BaseWebViewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.base_web_view, container, false)
 
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
     override fun onStart() {
         super.onStart()
         getWebView()?.let { webView ->
             initWebView(webView)
             webView.webViewClient = webViewClient
+            setJavascriptEnabledAssist(webView.settings)
         }
         getSwipeView()?.setOnRefreshListener {
             getWebView()?.reload()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+    private fun setJavascriptEnabledAssist(webSettings: WebSettings) {
+        webSettings.apply {
+            domStorageEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            displayZoomControls = false
         }
     }
 
