ae6554eec4e007b0d5a92f0a8bd8aaa61e434282
内田正輝 <masaki.u@arsaga.jp>
2022-04-01 19:42:29 +0900

    戻ってきた時に1回だけ実行する関数にsuspend関数を渡せるバージョンを追加作成

--- src/main/java/jp/co/arsaga/extensions/view/navigation/ArgsBackStackResult.kt
+++ src/main/java/jp/co/arsaga/extensions/view/navigation/ArgsBackStackResult.kt
@@ -3,7 +3,9 @@ package jp.co.arsaga.extensions.view.navigation
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.launch
 
 
 fun <T> Fragment.setBackStackResult(key: String, value: T) {
@@ -21,6 +23,15 @@ fun <T> Fragment.getBackStackResult(key: String): T? {
 fun <T> Fragment.getBackStackResultOnce(owner: LifecycleOwner, key: String, onChanged: (T) -> Unit) {
     getBackStackResultLiveData<T>(key)?.observe(owner) {
         onChanged.invoke(it)
+        findNavController().currentBackStackEntry?.savedStateHandle?.remove<T>(key)
+    }
+}
+
+suspend fun <T> Fragment.getBackStackResultOnceAsync(owner: LifecycleOwner, key: String, onChanged: suspend (T) -> Unit) {
+    getBackStackResultLiveData<T>(key)?.observe(owner) {
+        lifecycleScope.launch {
+            onChanged.invoke(it)
+        }
         findNavController().currentBackStackEntry?.savedStateHandle?.remove<T>(key)
     }
 }
