a0bfc4a941cf7ded6e7e40b95cf20f8d7a597e2e
Maski-U <kooo5252@gmail.com>
2022-12-06 14:27:05 +0900

    UCropで画像編集する機能を抽象化

--- src/main/java/jp/co/arsaga/extensions/view/fragment/ResultLauncher.kt
+++ src/main/java/jp/co/arsaga/extensions/view/fragment/ResultLauncher.kt
@@ -1,20 +1,73 @@
 package jp.co.arsaga.extensions.view.fragment
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.yalantis.ucrop.UCrop
 
 
-class ImagePicker(
+interface ResultLauncher {
+    fun pickData()
+}
+
+sealed class DataPicker(
     fragment: Fragment,
-    uriCallback: (Uri?) -> Unit
-) {
+    uriCallback: (Uri?) -> Unit,
+) : ResultLauncher {
     private val resultLauncher = fragment.registerForActivityResult(
         ActivityResultContracts.GetContent(),
         uriCallback
     )
 
-    fun pickImage() {
-        resultLauncher.launch("image/*")
+    protected abstract val input: String?
+    override fun pickData() {
+        resultLauncher.launch(input)
     }
+}
+
+class Picker(
+    fragment: Fragment,
+    uriCallback: (Uri?) -> Unit,
+) : DataPicker(fragment, uriCallback) {
+    override val input: String = "image/*"
+}
+
+fun Fragment.imageCrop(
+    saveUri: Context.() -> Uri,
+    config: Context.(UCrop.Options) -> Unit,
+    callback: (Uri?) -> Unit,
+    resultLauncher: ((Uri?) -> Unit) -> ResultLauncher,
+): ResultLauncher {
+    val cropLauncher = registerForActivityResult(CropImage(saveUri, config)) {
+        callback(it)
+    }
+    return resultLauncher {
+        it ?: return@resultLauncher
+        cropLauncher.launch(it)
+    }
+}
+
+private class CropImage(
+    private val saveUri: Context.() -> Uri,
+    private val config: Context.(UCrop.Options) -> Unit,
+) : ActivityResultContract<Uri, Uri?>() {
+
+    override fun createIntent(
+        context: Context,
+        input: Uri,
+    ): Intent = UCrop.of(input, saveUri(context)).apply {
+        UCrop.Options()
+            .also { config(context, it) }
+            .let { withOptions(it) }
+    }.getIntent(context)
+
+    override fun parseResult(
+        resultCode: Int, intent: Intent?,
+    ): Uri? = intent
+        ?.takeIf { resultCode == Activity.RESULT_OK }
+        ?.let { UCrop.getOutput(it) }
 }
